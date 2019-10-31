0bcb5afe1d989ed6b5fcaf99f43075052fd1c981
rerich3 <miroslav.imris@gmail.com>
2019-10-31 13:35:33 +0100

    domaciukol

---
+++ vlozit.php
@@ -0,0 +1,14 @@
+<?php
+if (!isset($_POST['Jméno'], $_POST['Vzkaz']))
+{echo 'Neplatný prístup!';}
+else {
+    $jmeno = $_POST['Jméno'];
+    $vzkaz = $_POST['Vzkaz'];
+$info = "<b>$jmeno</b><br>$vzkaz</hr>";
+$otvor = fopen('prispevky.txt', 'a');
+fwrite ($otvor, $info);
+fclose($otvor);
+echo 'Uložené!';
+}
+?>
+<a href="navstevni-kniha.php">Vratit sa.</a>
