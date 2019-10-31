cfcc5bfd252ebe2f1db285bc52c81c455ef4894e
rerich3 <miroslav.imris@gmail.com>
2019-10-31 11:58:16 +0100

    ukol1

--- ukol-1.php
+++ ukol-1.php
@@ -1,3 +1,12 @@
 <?php
+function minimum($a , $b)
+{
+    if (!is_numeric($a))
+    {echo 'chyba';}
+    elseif ($a > $b)
+    {return $b;}
+    elseif ($b > $a)
+    {return $a;}
+}
 
-echo 'Historie přístupů:<br>';
+echo minimum(56, 12.3);
